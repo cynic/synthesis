5a88ae01b47aceec7b7c6c73f4fe55b06abb51c3
YMM <cinyc.s@gmail.com>
2019-03-06 12:36:41 +0200

    Add Synthesis.fs

---
+++ Synthesis/Library.fs
@@ -0,0 +1,46 @@
+﻿module Synthesis
+
+let abelar _ =
+    failwith "Not implemented"
+
+let area _ _ =
+    failwith "Not implemented"
+
+let zollo _ =
+    failwith "Not implemented"
+
+let min _ _ =
+    failwith "Not implemented"
+
+let max _ _ =
+    failwith "Not implemented"
+
+let ofTime _ _ _ =
+    failwith "Not implemented"
+
+let toTime _ =
+    failwith "Not implemented"
+
+let digits _ =
+    failwith "Not implemented"
+
+let minmax _ =
+    failwith "Not implemented"
+
+let isLeap _ =
+    failwith "Not implemented"
+
+let month _ =
+    failwith "Not implemented"
+
+let toBinary _ =
+    failwith "Not implemented"
+
+let bizFuzz _ =
+    failwith "Not implemented"
+
+let monthDay _ _ =
+    failwith "Not implemented"
+
+let coord _ =
+    failwith "Not implemented"
